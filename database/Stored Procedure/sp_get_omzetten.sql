dc57900e7f17871ec58f517466bdf2d1c836a380
OdiMatar <340712@student.mboutrecht.nl>
2026-01-05 13:36:08 +0100

    1 sql voor omzet

---
+++ database/Stored Procedure/sp_get_omzetten.sql
@@ -0,0 +1,17 @@
+
+DROP PROCEDURE IF EXISTS sp_get_omzetten;
+DELIMITER $$
+
+CREATE PROCEDURE sp_get_omzetten()
+BEGIN
+    SELECT
+        id,
+        omschrijving,
+        klant_naam,
+        datum,
+        bedrag
+    FROM omzetten
+    ORDER BY datum DESC, id DESC;
+END $$
+
+DELIMITER ;
