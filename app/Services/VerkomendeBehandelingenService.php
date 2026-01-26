56da924c59f09597ae2ee954c766c2d01d3127a2
OdiMatar <340712@student.mboutrecht.nl>
2026-01-26 18:21:55 +0100

    aanpasing in sevices

--- app/Services/VerkomendeBehandelingenService.php
+++ app/Services/VerkomendeBehandelingenService.php
@@ -12,7 +12,17 @@ class VerkomendeBehandelingenService
             ->join('people as p', 'p.user_id', '=', 'a.dentist_id')
             ->where('a.date', '>=', now()->toDateString())
             ->where('a.is_actief', 1)
-            ->select('a.date', 'a.status', 'p.voornaam', 'p.tussenvoegsel', 'p.achternaam')
+            ->select(
+                'a.date',
+                'a.status',
+                'a.opmerking',
+                'p.voornaam',
+                'p.tussenvoegsel',
+                'p.achternaam'
+            )
+            ->selectRaw("'10:00' as tijd")
+            ->selectRaw("'Afspraak' as afspraak")
+            ->selectRaw("'Controle' as behandeling")
             ->orderBy('a.date')
             ->get();
     }
