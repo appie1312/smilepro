89b03bfaa7a5cf6b629b1c2536f308f91d4bfed4
appie1312 <agoeswag@gmil.com>
2026-01-27 09:09:20 +0100

    deels

--- resources/views/invoices/manage.blade.php
+++ resources/views/invoices/manage.blade.php
@@ -38,6 +38,9 @@
                                         <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
                                             Omschrijving
                                         </th>
+                                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
+                                            Acties
+                                        </th>
                                     </tr>
                                 </thead>
                                 <tbody class="bg-white dark:bg-gray-800 divide-y divide-gray-200 dark:divide-gray-700">
@@ -72,6 +75,12 @@
                                             </td>
                                             <td class="px-6 py-4 text-sm text-gray-900 dark:text-gray-100">
                                                 {{ $invoice->description ?? 'Geen omschrijving' }}
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                                                <a href="{{ route('invoices.edit', $invoice) }}"
+                                                   class="text-indigo-600 hover:text-indigo-900 dark:text-indigo-400 dark:hover:text-indigo-300">
+                                                    Bewerken
+                                                </a>
                                             </td>
                                         </tr>
                                     @endforeach
